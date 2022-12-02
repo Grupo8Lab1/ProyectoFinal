a54c0f513d987f5219f5cd743beff3b51bd19282
Ni-luteno <ni.luteno@gmail.com>
2022-12-01 21:22:19 -0300

    Update ProyectoFinalUniversidadG8.java

--- ProyectoFinalUniversidadG8/src/TestUMLs/ProyectoFinalUniversidadG8.java
+++ ProyectoFinalUniversidadG8/src/TestUMLs/ProyectoFinalUniversidadG8.java
@@ -22,9 +22,27 @@ public class ProyectoFinalUniversidadG8 {
 
     public static void main(String[] args) {
 
+        //NOTA: AGREGAR LA LIBRERIA DE mariadb-java-client.2.3.0.jar
+        ClienteData cd = new ClienteData();
+        BicicletaData bd = new BicicletaData();
+        RepuestoData repud = new RepuestoData();
+        ServicioData sd = new ServicioData();
+        ItemRepuestoData irepud = new ItemRepuestoData();
+        ReparacionData repad = new ReparacionData();
+
+        System.out.println(cd.obtenerClientes());
+        System.out.println(bd.obtenerBicicletas());
+        System.out.println(repud.obtenerRepuestos());
+        System.out.println(irepud.obtenerItemRepuestos());
+        System.out.println(sd.obtenerServicios());
+        System.out.println(repad.obtenerReparaciones());
 
         /*
-        Funcionan: 
+        
+        Testing realizado con anterioridad, falta chequear la implementación de los ultimos metodos
+        (Bicicleta por Cliente, repuesto por Descripcion, y reparacion por Fecha)
+        
+        Metodos basicos funcionando: 
         
         - ClienteData:
         todos.
