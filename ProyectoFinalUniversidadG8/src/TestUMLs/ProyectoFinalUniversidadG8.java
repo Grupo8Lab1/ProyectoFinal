241ae43134eccab6ec3f910ba1ddd3af3f79598e
Ni-luteno <ni.luteno@gmail.com>
2022-12-02 16:43:09 -0300

    Merge arreglado

--- ProyectoFinalUniversidadG8/src/TestUMLs/ProyectoFinalUniversidadG8.java
+++ ProyectoFinalUniversidadG8/src/TestUMLs/ProyectoFinalUniversidadG8.java
@@ -4,7 +4,6 @@
  */
 package TestUMLs;
 
-
 import Entidades.Bicicleta;
 import Entidades.Cliente;
 import Entidades.Reparacion;
@@ -16,50 +15,29 @@ import Persistencia.ItemRepuestoData;
 import Persistencia.ReparacionData;
 import Persistencia.RepuestoData;
 import Persistencia.ServicioData;
-<<<<<<< Updated upstream
+
 import java.util.ArrayList;
-=======
+
 import java.sql.Date;
 import java.time.LocalDate;
->>>>>>> Stashed changes
 
 public class ProyectoFinalUniversidadG8 {
 
-        public static ClienteData cd = new ClienteData();
-        public static BicicletaData bd = new BicicletaData();
-        public static RepuestoData repud = new RepuestoData();
-        public static ServicioData sd = new ServicioData();
-        public static ItemRepuestoData irepud = new ItemRepuestoData();
-        public static ReparacionData repad = new ReparacionData();
-        public static ArrayList<Cliente> listaClientes;
-        public static ArrayList<Bicicleta> listaBicicleta;
-        public static ArrayList<Repuesto> listaRepuesto;
-        public static ArrayList<Servicio> listaServicio;
-        public static ArrayList<Reparacion> listaReparacion;
-        
-    public static void main(String[] args) {
-        
+    public static ClienteData cd = new ClienteData();
+    public static BicicletaData bd = new BicicletaData();
+    public static RepuestoData repud = new RepuestoData();
+    public static ServicioData sd = new ServicioData();
+    public static ItemRepuestoData irepud = new ItemRepuestoData();
+    public static ReparacionData repad = new ReparacionData();
+    public static ArrayList<Cliente> listaClientes;
+    public static ArrayList<Bicicleta> listaBicicleta;
+    public static ArrayList<Repuesto> listaRepuesto;
+    public static ArrayList<Servicio> listaServicio;
+    public static ArrayList<Reparacion> listaReparacion;
 
+    public static void main(String[] args) {
 
         //NOTA: AGREGAR LA LIBRERIA DE mariadb-java-client.2.3.0.jar
-<<<<<<< Updated upstream
-       
-=======
-        /*  ClienteData cd = new ClienteData();
-        BicicletaData bd = new BicicletaData();
-        RepuestoData repud = new RepuestoData();
-        ServicioData sd = new ServicioData();
-        ItemRepuestoData irepud = new ItemRepuestoData();
-         */ ReparacionData repad = new ReparacionData();
->>>>>>> Stashed changes
-
-        /*System.out.println(cd.obtenerClientes());
-        System.out.println(bd.obtenerBicicletas());
-        System.out.println(repud.obtenerRepuestos());
-        System.out.println(irepud.obtenerItemRepuestos());
-        System.out.println(sd.obtenerServicios());
-        System.out.println(repad.obtenerReparaciones());
-         */
         LocalDate fecha = LocalDate.now();
         repad.obtenerReparacionesPorFecha(fecha.minusDays(5), fecha);
 
